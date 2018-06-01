828c041bdd68704628a2d7fccc5982c2d333f980
Juan Fernando Tamayo <j98fer@gmail.com>
2018-06-01 14:15:01 -0500

    Create selected blocks

--- Proyecto/app/src/main/java/co/edu/udea/ubiu/MainActivity.java
+++ Proyecto/app/src/main/java/co/edu/udea/ubiu/MainActivity.java
@@ -2,10 +2,8 @@ package co.edu.udea.ubiu;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,10 +13,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import co.edu.udea.ubiu.fragments.BlocksFragment;
 import co.edu.udea.ubiu.fragments.GMapFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+                    BlocksFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        navigationView.setCheckedItem(R.id.nav_gallery);
+        navigationView.setCheckedItem(R.id.nav_map);
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.content_frame, new GMapFragment());
         fragmentTransaction.commit();
@@ -83,11 +83,11 @@ public class MainActivity extends AppCompatActivity
 
         int id = item.getItemId();
 
-        if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_map) {
             fm.beginTransaction().replace(R.id.content_frame, new GMapFragment()).commit();
 
-        } else if (id == R.id.nav_slideshow) {
-
+        } else if (id == R.id.nav_blocks) {
+            fm.beginTransaction().replace(R.id.content_frame, new BlocksFragment()).commit();
 
         } else if (id == R.id.nav_manage) {
 
@@ -100,5 +100,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
--- Proyecto/app/src/main/java/co/edu/udea/ubiu/fragments/GMapFragment.java
+++ Proyecto/app/src/main/java/co/edu/udea/ubiu/fragments/GMapFragment.java
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,83 +29,69 @@ import co.edu.udea.ubiu.R;
 public class GMapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
     LatLng medellin = new LatLng(6.2674, -75.5688);
     LatLng aero = new LatLng(6.270283, -75.569916);
     LatLng ing = new LatLng(6.269368, -75.566685);
     LatLng quilla = new LatLng(6.265207, -75.567844);
     LatLng humanas = new LatLng(6.265601, -75.570835);
 
-    MarkerOptions bl1,bl2,bl3,bl4,bl5,bl6,bl7,bl8,bl9,bl20,bl10,
-            bl11,bl12,bl13,bl14,bl15,bl16,bl17,bl18,bl19,bl21,bl22,bl23,bl24,bl25,bl26,
-            bl27,bl28,bl29;
+    MarkerOptions[] blocks = new MarkerOptions[29];
+    float generalAlpha = 0.3f;
+    float[][] latLngs = {
+            {6.2655f,-75.5686f},
+            {6.2657f,-75.5680f},
+            {6.2660f,-75.5682f},
+            {6.2663f,-75.5680f},
+            {6.2666f,-75.5683f},
+            {6.2664f,-75.5688f},
+            {6.2659f,-75.5688f},
+            {6.2668f,-75.5692f},
+            {6.2657f,-75.5690f},
+            {6.2663f,-75.5699f},
+            {6.2667f,-75.5698f},
+            {6.2669f,-75.5703f},
+            {6.2665f,-75.5704f},
+            {6.2661f,-75.5704f},
+            {6.2673f,-75.5698f},
+            {6.2674f,-75.5683f},
+            {6.2669f,-75.5690f},
+            {6.2677f,-75.5673f},
+            {6.2682f,-75.5673f},
+            {6.2683f,-75.5680f},
+            {6.2679f,-75.5680f},
+            {6.2681f,-75.5686f},
+            {6.2679f,-75.5693f},
+            {6.2681f,-75.5698f},
+            {6.2677f,-75.5698f},
+            {6.2684f,-75.5691f},
+            {6.2694f,-75.5682f},
+            {6.2690f,-75.5670f},
+            {6.2687f,-75.5669f}
+    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gmap, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_gmap, container, false);
+
+
+        return v;
+
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
-        bl1 = new MarkerOptions().position(new LatLng(6.2655,
-                -75.5686)).title("Bloque 1").alpha(0.3f);
-        bl2 = new MarkerOptions().position(new LatLng(6.2657,
-                -75.5680)).title("Bloque 2").alpha(0.3f);
-        bl3 = new MarkerOptions().position(new LatLng(6.2660,
-                -75.5682)).title("Bloque 3").alpha(0.3f);
-        bl4 = new MarkerOptions().position(new LatLng(6.2663,
-                -75.5680)).title("Bloque 4").alpha(0.3f);
-        bl5 = new MarkerOptions().position(new LatLng(6.2666,
-                -75.5683)).title("Bloque 5").alpha(0.3f);
-        bl6 = new MarkerOptions().position(new LatLng(6.2664,
-                -75.5688)).title("Bloque 6").alpha(0.3f);
-        bl7 = new MarkerOptions().position(new LatLng(6.2659,
-                -75.5688)).title("Bloque 7").alpha(0.3f);
-        bl8 = new MarkerOptions().position(new LatLng(6.2668,
-                -75.5692)).title("Bloque 8").alpha(0.3f);
-        bl9 = new MarkerOptions().position(new LatLng(6.2657,
-                -75.5690)).title("Bloque 9").alpha(0.3f);
-        bl10 = new MarkerOptions().position(new LatLng(6.2663,
-                -75.5699)).title("Bloque 10").alpha(0.3f);
-        bl11 = new MarkerOptions().position(new LatLng(6.2667,
-                -75.5698)).title("Bloque 11").alpha(0.3f);
-        bl12 = new MarkerOptions().position(new LatLng(6.2669,
-                -75.5703)).title("Bloque 12").alpha(0.3f);
-        bl13 = new MarkerOptions().position(new LatLng(6.2665,
-                -75.5704)).title("Bloque 13").alpha(0.3f);
-        bl14 = new MarkerOptions().position(new LatLng(6.2661,
-                -75.5704)).title("Bloque 14").alpha(0.3f);
-        bl15 = new MarkerOptions().position(new LatLng(6.2673,
-                -75.5698)).title("Bloque 15").alpha(0.3f);
-        bl16 = new MarkerOptions().position(new LatLng(6.2674,
-                -75.5683)).title("Bloque 16").alpha(0.3f);
-        bl17 = new MarkerOptions().position(new LatLng(6.2669,
-                -75.5690)).title("Bloque 17").alpha(0.3f);
-        bl18 = new MarkerOptions().position(new LatLng(6.2677,
-                -75.5673)).title("Bloque 18").alpha(0.3f);
-        bl19 = new MarkerOptions().position(new LatLng(6.2682,
-                -75.5673)).title("Bloque 19").alpha(0.3f);
-        bl20 = new MarkerOptions().position(new LatLng(6.2683,
-                -75.5680)).title("Bloque 20").alpha(0.3f);
-        bl21 = new MarkerOptions().position(new LatLng(6.2679,
-                -75.5680)).title("Bloque 21").alpha(0.3f);
-        bl22 = new MarkerOptions().position(new LatLng(6.2681,
-                -75.5686)).title("Bloque 22").alpha(0.3f);
-        bl23 = new MarkerOptions().position(new LatLng(6.2679,
-                -75.5693)).title("Bloque 23").alpha(0.3f);
-        bl24 = new MarkerOptions().position(new LatLng(6.2681,
-                -75.5698)).title("Bloque 24").alpha(0.3f);
-        bl25 = new MarkerOptions().position(new LatLng(6.2677,
-                -75.5698)).title("Bloque 25").alpha(0.3f);
-        bl26 = new MarkerOptions().position(new LatLng(6.2684,
-                -75.5691)).title("Bloque 26").alpha(0.3f);
-        bl27 = new MarkerOptions().position(new LatLng(6.2694,
-                -75.5682)).title("Bloque 27").alpha(0.3f);
-        bl28 = new MarkerOptions().position(new LatLng(6.2690,
-                -75.5670)).title("Bloque 28").alpha(0.3f);
-        bl29 = new MarkerOptions().position(new LatLng(6.2687,
-                -75.5669)).title("Bloque 29").alpha(0.3f);
+
+        for(int i = 0; i < blocks.length; i++){
+
+            blocks[i] = new MarkerOptions().position(new LatLng(latLngs[i][0],latLngs[i][1])).title("Bloque " + String.valueOf(i+1)).alpha(generalAlpha);
+        }
+
+
 
         MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         fragment.getMapAsync(this);
@@ -114,36 +99,15 @@ public class GMapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
-        mMap.addMarker(bl1);
-        mMap.addMarker(bl2);
-        mMap.addMarker(bl3);
-        mMap.addMarker(bl4);
-        mMap.addMarker(bl5);
-        mMap.addMarker(bl6);
-        mMap.addMarker(bl7);
-        mMap.addMarker(bl8);
-        mMap.addMarker(bl9);
-        mMap.addMarker(bl10);
-        mMap.addMarker(bl11);
-        mMap.addMarker(bl12);
-        mMap.addMarker(bl13);
-        mMap.addMarker(bl14);
-        mMap.addMarker(bl15);
-        mMap.addMarker(bl16);
-        mMap.addMarker(bl17);
-        mMap.addMarker(bl18);
-        mMap.addMarker(bl19);
-        mMap.addMarker(bl20);
-        mMap.addMarker(bl21);
-        mMap.addMarker(bl22);
-        mMap.addMarker(bl23);
-        mMap.addMarker(bl24);
-        mMap.addMarker(bl25);
-        mMap.addMarker(bl26);
-        mMap.addMarker(bl27);
-        mMap.addMarker(bl28);
-        mMap.addMarker(bl29);
+
+        for(int i = 0; i < blocks.length; i++){
+
+            mMap.addMarker(blocks[i]);
+
+        }
+
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         CameraPosition inicialPos = CameraPosition.builder().target(medellin).zoom(17).tilt(0).build();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(inicialPos));
